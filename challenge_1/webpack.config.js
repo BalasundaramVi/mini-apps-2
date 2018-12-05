7b8fded2c663b5ab98709dbb4432bdf6438eb98c
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-12-05 10:33:38 -0800

    Create boilerplate ReactJS app w/ webpack & eslint

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, './client/src/app.jsx'),
+  output: {
+    filename: 'c1_bundle.js',
+    path: path.join(__dirname, './client/public'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: path.join(__dirname, './client/src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
