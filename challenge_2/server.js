4cbb8ba12be49511ac4c2d015dd0df41e56a5f22
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-12-06 10:47:09 -0800

    Create npm script for env variables postinstall

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+
+const path = require('path');
+
+const parser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const { PORT } = process.env;
+
+app.use(parser.json());
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, './client/public')));
 
 
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}...`);
+});
