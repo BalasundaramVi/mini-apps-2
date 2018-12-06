cbaaaa55ed6d2d1cd3a34d542c139a4588ed501d
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-12-06 10:24:33 -0800

    Create boilerplate configurations

---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, './client/src/index.jsx'),
+  output: {
+    filename: 'c2_bundle.js',
+    path: path.join(__dirname, './client/public'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: path.join(__dirname, './client/src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
