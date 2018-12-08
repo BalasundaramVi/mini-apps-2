bdee3eb48d6d935c89c79edd968f962d9a6966cb
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-12-08 12:24:51 -0800

    Create boilerplate react w/ express/eslint/babel

---
+++ challenge_4/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "parser": "babel-eslint",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "rules": {
+    "no-console": 0,
+  },
+  "env": {
+    "browser": true,
+  },
+};
